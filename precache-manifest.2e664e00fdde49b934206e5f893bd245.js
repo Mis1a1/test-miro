ee588871bb97a0de3aa7ce9fb3db35f948d5118a
Mis1a1 <mislaswaggg@gmail.com>
2020-06-26 18:04:08 +0500

    Updates

---
+++ precache-manifest.2e664e00fdde49b934206e5f893bd245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e7b8780a3e3c09caf59c05073f6fe8",
+    "url": "/test-miro/index.html"
+  },
+  {
+    "revision": "20b770942db452c87460",
+    "url": "/test-miro/static/css/main.9073b9dc.chunk.css"
+  },
+  {
+    "revision": "2e1a4dd524f704bb16c2",
+    "url": "/test-miro/static/js/2.2fe1985a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-miro/static/js/2.2fe1985a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b770942db452c87460",
+    "url": "/test-miro/static/js/main.bd12721f.chunk.js"
+  },
+  {
+    "revision": "47436e201c444f25cc5a",
+    "url": "/test-miro/static/js/runtime-main.1a6239d3.js"
+  }
+]);
